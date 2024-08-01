cda5968814f21963f4602cdc21a9e59d66947888
GenissonEmilio <esgenisson@gmail.com>
2024-08-01 20:05:43 -0300

    update

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+  document.querySelector('.botao').addEventListener('click', () => {
+    alert('Tá achando que pode esquecer a senha Neggen')
+  })
+})
