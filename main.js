8476f50ca4f2c90c7401aaef8ebd0134f62f0818
GenissonEmilio <esgenisson@gmail.com>
2024-08-09 19:26:49 -0300

    update

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#formu');
+  const esqueceu = document.querySelector('.botao');
   
-  document.querySelector('.botao').addEventListener('click', () => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    if (event) {
+      window.location.href = 'teste.html'
+    }
+    
+  })
+  
+  esqueceu.addEventListener('click', () => {
     alert('Tá achando que pode esquecer a senha Neggen')
   })
+  
+  
 })
